2f5c2187c462c57c6d9cb29984d352b41b476d0f
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-05-30 13:45:12 +0530

    Refactor unsplashService.js to use axios.create for API requests, add searchImages function for searching photos with pagination, and getRandomImages function for fetching random photos

---
+++ src/services/unsplashService.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: 'https://api.unsplash.com',
+    headers: {
+      Authorization: 'Client-ID 5eUslWw8uROEku_R-kePf4MvmeaGLB6wPM8501fLsl4',
+    },
+  });;
+  
+  export const searchImages = async (query, page) => {
+    try {
+      const response = await api.get('/search/photos', {
+        params: {
+          query,
+          page,
+          per_page: 6,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching images:', error);
+      throw error;
+    }
+  };
+  
+  export const getRandomImages = async (count) => {
+    try {
+      const response = await api.get('/photos/random', {
+        params: {
+          count,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching random images:', error);
+      throw error;
+    }
+  };
+  
