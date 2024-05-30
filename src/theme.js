20f90f92b4dd5bfa614683038efca5895ab90fca
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-05-30 12:00:46 +0530

    chore : "Updated package-lock.json and package.json to include new dependencies axios, react-router-dom, and @remix-run/router"

---
+++ src/theme.js
@@ -0,0 +1,37 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Arial, sans-serif',
+    fontSize: 14,
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+    fontWeightBold: 700,
+    h1: {
+      fontSize: '2rem',
+      fontWeight: 700,
+    },
+    h2: {
+      fontSize: '1.75rem',
+      fontWeight: 500,
+    },
+    body1: {
+      fontSize: '1rem',
+    },
+    body2: {
+      fontSize: '0.875rem',
+    },
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+});
+
+export default theme;
