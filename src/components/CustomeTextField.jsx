77871ec05748abfe87cfbe785285572321b29522
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-05-30 12:07:07 +0530

    feat : custom textfield component added

---
+++ src/components/CustomeTextField.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import TextField from "@mui/material/TextField";
+
+const CustomTextField = (props) => {
+  return <TextField variant="outlined" {...props} />;
+};
+
+export default CustomTextField;
